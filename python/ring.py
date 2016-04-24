1a8940e6b6a075eb0df500204e7235ff79cb15d1
Pavel Fokin <fokinpv@gmail.com>
2016-04-24 10:28:24 +0300

    [py] Added poly_type option to BoolPolyRing constructor.

--- python/ring.py
+++ python/ring.py
@@ -2,14 +2,18 @@
 """
 from monom import Monom
 from poly import Poly
+from zdd import ZDD
 
 
 class BoolPolyRing(object):
 
-    def __init__(self, n):
+    def __init__(self, n, poly_type="list"):
         Monom.size = n
 
-        self.gens = [
-            Poly([Monom(vars=[i])])
-            for i in range(n)
-        ]
+        if poly_type == "list":
+            self.gens = [
+                Poly([Monom(vars=[i])])
+                for i in range(n)
+            ]
+        elif poly_type == "zdd":
+            self.gens = [ZDD(i) for i in range(n)]
--- python/tests/test_ring.py
+++ python/tests/test_ring.py
@@ -1,14 +1,23 @@
 import unittest
 
+from zdd import ZDD
 from ring import BoolPolyRing
 
 
 class TestBoolPolyRing(unittest.TestCase):
 
-    def test_ring(self):
-        ring_size = 5
+    def setUp(self):
+        self.ring_size = 5
 
-        B = BoolPolyRing(ring_size)
+    def test_ring(self):
+        B = BoolPolyRing(self.ring_size)
         x1, x2, x3, x4, x5 = B.gens
 
-        self.assertEqual(ring_size, len(B.gens))
+        self.assertEqual(self.ring_size, len(B.gens))
+
+    def test_ring_zdd(self):
+        B = BoolPolyRing(self.ring_size, poly_type="zdd")
+        x1, x2, x3, x4, x5 = B.gens
+
+        for var in B.gens:
+            self.assertTrue(isinstance(var, ZDD))
