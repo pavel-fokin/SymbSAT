87fb142743e0ca37eb039201f9a14f4fded64db5
Pavel Fokin <fokinpv@gmail.com>
2017-03-25 16:23:15 +0400

    [c++] Remove old sources

--- symbsat-cpp/gb.h
+++
@@ -1,9 +0,0 @@
-#ifndef GB_H
-#define GB_H
-
-#include "poly.h"
-
-std::vector<Poly> autoreduce(const std::vector<Poly>&);
-std::vector<Poly> buchberger(const std::vector<Poly>&, const int);
-
-#endif // GB_H
